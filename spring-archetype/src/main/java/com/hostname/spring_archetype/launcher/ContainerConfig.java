5b38c5d8164f323f71ec7c07e2197314fb5593d9
tangdong <tangdong@kftpay.com.cn>
2019-10-09 17:29:26 +0800

    Add spring container launcher

---
+++ spring-archetype/src/main/java/com/hostname/spring_archetype/launcher/ContainerConfig.java
@@ -0,0 +1,24 @@
+package com.hostname.spring_archetype.launcher;
+
+/**
+ * <p>启动spring容器时的配置文件路径配置类</p>
+ *
+ * @author <a href="mailto:tangdong@kftpay.com.cn">Taunton</a>
+ * @date Created in 2019-10-09 15:18
+ * @since
+ */
+public class ContainerConfig {
+
+    /**
+     * 应用上下文配置文件路径
+     */
+    private String appContextPath;
+
+    public String getAppContextPath() {
+        return appContextPath;
+    }
+
+    public void setAppContextPath(String appContextPath) {
+        this.appContextPath = appContextPath;
+    }
+}
