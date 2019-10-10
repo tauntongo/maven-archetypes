739e266c5994195824c49d01add8f185ee2ae69b
tangdong <tangdong@kftpay.com.cn>
2019-10-10 15:31:10 +0800

    super junit test class

---
+++ spring-archetype/src/test/java/com/hostname/spring_archetype/test/BaseJunitTest.java
@@ -0,0 +1,51 @@
+package com.hostname.spring_archetype.test;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.result.*;
+
+/**
+ * <p>Spring容器单元测试基础类</p>
+ *
+ * @author <a href="mailto:tangdong@kftpay.com.cn">Taunton</a>
+ * @date Created in 2019-10-10 14:49
+ * @since
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath*:spring-archetype-dependencies.xml"})
+public class BaseJunitTest {
+
+    protected Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @Before
+    public void setup() {
+        //init operation...
+    }
+
+    @Ignore
+    public StatusResultMatchers status() {
+        return MockMvcResultMatchers.status();
+    }
+
+    @Ignore
+    public ViewResultMatchers view() {
+        return MockMvcResultMatchers.view();
+    }
+
+    @Ignore
+    public ModelResultMatchers model() {
+        return MockMvcResultMatchers.model();
+    }
+
+    @Ignore
+    public ResultHandler print() {
+        return MockMvcResultHandlers.print();
+    }
+
+}
